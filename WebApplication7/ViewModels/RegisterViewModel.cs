e411716de9bc052d8f38d0d4e1308df5dd08eb39
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-09-27 03:03:15 +0600

    add register view model

---
+++ WebApplication7/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication7.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+    
+    [Required]
+    [Display(Name = "Дата рождения")]
+    public DateTime DateOfBirth { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Пароль")]
+    public string Password { get; set; }
+    
+    [Required]
+    [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Подтвердить пароль")]
+    public string PasswordConfirm { get; set; }
+}
