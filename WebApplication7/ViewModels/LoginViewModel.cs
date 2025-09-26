1fac3696a4c457255278611c0e8b5b2ee728ad4b
Taiyrbek Anarbekov <an.taku2455@gmail.com>
2025-09-27 03:03:26 +0600

    add login view model

---
+++ WebApplication7/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication7.ViewModels;
+
+public class LoginViewModel
+{
+    [Required]
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Пароль")]
+    public string Password { get; set; } = null!;
+
+    [Display(Name = "Запомнить?")]
+    public bool RememberMe { get; set; }
+    
+    public string? ReturnUrl { get; set; }
+}
